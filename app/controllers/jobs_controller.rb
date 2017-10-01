8e5ae201fac9e320e38fadfb628c97c772a64418
Severin Wiggenhorn <severin.randall@gmail.com>
2017-10-01 14:46:33 -0700

    add route and controller for new action

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -8,4 +8,8 @@ class JobsController < ApplicationController
     @job = Job.find(params[:id])
   end
 
+  def new
+
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   get "/jobs", to: "jobs#index"
 
+  get "jobs/new", to: "jobs#new"
+
   get "/jobs/:id", to: "jobs#show", as: "job"
 end
