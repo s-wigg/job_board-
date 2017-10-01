c3c2c2ef52d6227d22b9fa3b713aec577ef0be61
Severin Wiggenhorn <severin.randall@gmail.com>
2017-10-01 14:29:54 -0700

    add the job show action

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get "/jobs", to: "jobs#index"
+
+  get "/jobs/:id", to: "jobs#show"
 end
